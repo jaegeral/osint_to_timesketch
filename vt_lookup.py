723c7e1051dc746e6831ebbfaeb3d1172b71f0b2
jaegeral <alexander@jaeger.org>
2018-12-28 13:02:30 +0100

    some error handling to not quit the script.

--- vt_lookup.py
+++ vt_lookup.py
@@ -51,54 +51,63 @@ def investigate_domain(domain_to_investigate):
     response = vt.get_domain_report(domain_to_investigate)
     if response['response_code'] == 200:
         # print(message, timestamp, datetime, timestamp_desc, extra_field_1, extra_field_2)
-        for element in response['results']['resolutions']:
-            timestamp_desc = "VT_Domain_to_ip"
-            datetime_new = convert_date_to_datetime(element['last_resolved'])
-            ip = element['ip_address']
+        try:
+            for element in response['results']['resolutions']:
+                timestamp_desc = "VT_Domain_to_ip"
+                datetime_new = convert_date_to_datetime(element['last_resolved'])
+                ip = element['ip_address']
 
-            timestamp = convert_date_to_timestamp(element['last_resolved'])
-            message = "Domain " + domain_to_investigate + " was resolving to " + ip
+                timestamp = convert_date_to_timestamp(element['last_resolved'])
+                message = "Domain " + domain_to_investigate + " was resolving to " + ip
 
-            print(message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution')
-            csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution'])
+                print(message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution')
+                csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution'])
+        except:
+            print("Error with domain "+domain_to_investigate)
+            print(json.dumps(response, sort_keys=False, indent=4))
     else:
         print(json.dumps(response, sort_keys=False, indent=4))
 
 
 def investigate_ip(ip_to_investigate):
     response = vt.get_ip_report(ip_to_investigate)
+    try:
+        if response['response_code']==200:
 
-    if response['response_code']==200:
+            for element in response['results']['resolutions']:
+                timestamp_desc = "VT_ip_history"
+                datetime_new = convert_date_to_datetime(element['last_resolved'])
+                domain = element['hostname']
+                ip = ip_to_investigate
 
-        for element in response['results']['resolutions']:
-            timestamp_desc = "VT_ip_history"
-            datetime_new = convert_date_to_datetime(element['last_resolved'])
-            domain = element['hostname']
-            ip = ip_to_investigate
+                timestamp = convert_date_to_timestamp(element['last_resolved'])
+                message = "IP " + ip + " was resolving to " + domain
 
-            timestamp = convert_date_to_timestamp(element['last_resolved'])
-            message = "IP " + ip + " was resolving to " + domain
-
-            print(message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution')
-            csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution'])
-    else:
+                print(message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution')
+                csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, 'report', 'attribution'])
+        else:
+            print(json.dumps(response, sort_keys=False, indent=4))
+    except:
+        print("Issue with: "+ip_to_investigate)
         print(json.dumps(response, sort_keys=False, indent=4))
 
-
 def investigate_md5(md5):
+    try:
+        response = vt.get_file_report(md5)
+        if response['response_code']==200:
+            timestamp_desc = "VT_md5info"
+            datetime_new = convert_date_to_datetime(response['results']['scan_date'])
 
-    response = vt.get_file_report(md5)
-    if response['response_code']==200:
-        timestamp_desc = "VT_md5info"
-        datetime_new = convert_date_to_datetime(response['results']['scan_date'])
 
+            timestamp = convert_date_to_timestamp(response['results']['scan_date'])
+            message = "MD5 " + md5 + " was at least scanned by VT "
 
-        timestamp = convert_date_to_timestamp(response['results']['scan_date'])
-        message = "MD5 " + md5 + " was at least scanned by VT "
-
-        print(message, timestamp, datetime_new, timestamp_desc, report, attribution)
-        csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, report, attribution])
-    else:
+            print(message, timestamp, datetime_new, timestamp_desc, report, attribution)
+            csvwriter.writerow([message, timestamp, datetime_new, timestamp_desc, report, attribution])
+        else:
+            print(json.dumps(response, sort_keys=False, indent=4))
+    except:
+        print("Issue with "+md5)
         print(json.dumps(response, sort_keys=False, indent=4))
 
 
@@ -109,7 +118,7 @@ csvwriter = csv.writer(csvfile, delimiter=',',
 # header
 csvwriter.writerow(['message', 'timestamp', 'datetime', 'timestamp_desc', 'report', 'attribution'])
 
-filename = 'input2.txt'
+filename = 'input.txt'
 fin=open(filename,'r')
 for line in fin:
     line = line.replace('\n', '')
@@ -123,7 +132,7 @@ for line in fin:
         investigate_md5(line)
 
     # hacky way to not go into rate limit with VT
-    a = 30
+    a = 2
     while a != 0:
         import sys
         sys.stdout.write(str(a) + ' ')
